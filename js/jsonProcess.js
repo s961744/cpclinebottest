45ace6ad22dc34f9a8c0921f8723c9b6f0cd7ca2
s961744 <A0012272@cppcb.com.tw>
2018-07-12 11:48:46 +0800

    Add files via upload

--- js/jsonProcess.js
+++ js/jsonProcess.js
@@ -12,6 +12,7 @@ exports.getJsonFileArrayData = function (fileName) {
         fs.readFile('json/' + fileName + '.json', 'utf8', function (err, data) {
             if (err) reject(err);
             arr = JSON.parse(data);
+            console.log(JSON.stringify(arr));
             resolve(arr);
         });
     });
