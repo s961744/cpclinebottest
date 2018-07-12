a9e1dda3ed0159bc0ed4f43d1e216147b502f776
s961744 <A0012272@cppcb.com.tw>
2018-07-12 09:16:43 +0800

    Add files via upload

--- js/jsonProcess.js
+++ js/jsonProcess.js
@@ -5,17 +5,13 @@ const
     path = require('path');
 
 //get json file data
-exports.getJsonFileData = function (fileName) {
-    return new Promise(function (resolve, reject) {
-        var fs = require('fs');
-        var obj;
-        fs.readFile('json/' + fileName + '.json', 'utf8', function (data) {
-            obj = JSON.parse(data);
-            console.log("obj=" + JSON.stringify(obj));
-            resolve(obj);
-        }).catch(function (e) {
-            console.log("getJsonFileData error:" + e);
-            reject(e);
-        });
+exports.getJsonFileData = function(fileName) {
+    var fs = require('fs');
+    var obj;
+    fs.readFile('json/' + fileName + '.json', 'utf8', function (err, data) {
+        if (err) throw err;
+        obj = JSON.parse(data);
+        console.log("obj=" + JSON.stringify(obj));
+        return obj;
     });
 }
