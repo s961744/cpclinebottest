5abebbf36150f6a9a8a79d327e85e9e0b389101e
s961744 <A0012272@cppcb.com.tw>
2018-07-17 10:50:35 +0800

    Add files via upload

--- js/lineBotSdk.js
+++ js/lineBotSdk.js
@@ -14,84 +14,129 @@ const client = new line.Client(config);
 
 //回應訊息
 exports.replyMessage = function (replyToken, msg) {
-    client.replyMessage(replyToken, msg);
+    return new Promise(function (resolve, reject) {
+        client.replyMessage(replyToken, msg);
+        resolve(200);
+    }).catch(function (e) {
+        console.log('replyMessage error:' + e);
+        reject('replyMessage error:' + e);
+    });
 }
 
 //發送訊息
 exports.pushMessage = function (lineId, msg) {
-    client.pushMessage(lineId, msg);
+    return new Promise(function (resolve, reject) {
+        client.pushMessage(lineId, msg);
+        resolve(200);
+    }).catch(function (e) {
+        console.log('pushMessage error:' + e);
+        reject('pushMessage error:' + e);
+    });
 }
 
 //發送多對象訊息
 exports.multicast = function (ids, msg) {
-    client.multicast(ids, msg);
+    return new Promise(function (resolve, reject) {
+        client.multicast(ids, msg);
+        resolve(200);
+    }).catch(function (e) {
+        console.log('multicast error:' + e);
+        reject('multicast error:' + e);
+    });
 }
 
 //取得Line暱稱
-exports.getDisplayName = function(userId) {
-    client.getProfile(userId).then(function (profile) {
-        return profile.displayName;
+exports.getDisplayName = function (userId) {
+    return new Promise(function (resolve, reject) {
+        client.getProfile(userId).then(function (profile) {
+            resolve(profile.displayName);
+        });
     }).catch(function (e) {
-        console.log(userId + " getDisplayName error:" + e);
+        console.log('getDisplayName error:' + e);
+        reject('getDisplayName error:' + e);
     });
 }
 
 //取得image, video, audio訊息中的stream資料
 exports.getMessageContent = function (messageId) {
-    client.getMessageContent(messageId).then(function (stream) {
-        return stream;
+    return new Promise(function (resolve, reject) {
+        client.getMessageContent(messageId).then(function (stream) {
+            resolve(stream);
+        });
     }).catch(function (e) {
-        console.log(messageId + " getMessageContent error:" + e);
+        console.log('getMessageContent error:' + e);
+        reject('getMessageContent error:' + e);
     });
 }
 
 //建立richMenu
 exports.createRichMenu = function (rm) {
-    client.createRichMenu(rm).then(function (richMenuID) {
-        return richMenuID;
+    return new Promise(function (resolve, reject) {
+        client.createRichMenu(rm).then(function (richMenuID) {
+            resolve(richMenuID);
+        });
     }).catch(function (e) {
-        console.log("createRichMenu error:" + e);
+        console.log('createRichMenu error:' + e);
+        reject('createRichMenu error:' + e);
     });
 }
 
 //綁定rmName.png圖片給richMenuId
-exports.createRichMenu = function (richMenuId, buffer) {
-    client.setRichMenuImage(richMenuId, buffer).then(function () {
+exports.setRichMenuImage = function (richMenuId, buffer) {
+    return new Promise(function (resolve, reject) {
+        client.setRichMenuImage(richMenuId, buffer).then(function () {
+            resolve(200);
+        });
     }).catch(function (e) {
-        console.log(richMenuId + " setRichMenuImage error:" + e);
+        console.log(richMenuId + ' setRichMenuImage error:' + e);
+        reject(richMenuId + ' setRichMenuImage error:' + e);
     });
 }
 
 //綁定richMenuId給UserId
 exports.linkRichMenuToUser = function (userId, richMenuId) {
-    client.linkRichMenuToUser(userId, richMenuId).then(function () {
+    return new Promise(function (resolve, reject) {
+        client.linkRichMenuToUser(userId, richMenuId).then(function () {
+            resolve(200);
+        });
     }).catch(function (e) {
-        console.log(richMenuId + " linkRichMenuToUser " + userId + " error:" + e);
+        console.log(richMenuId + ' linkRichMenuToUser ' + userId + ' error:' + e);
+        reject(richMenuId + ' linkRichMenuToUser ' + userId + ' error:' + e);
     });
 }
 
 //取得UserId的richMenuId
 exports.getRichMenuIdOfUser = function (userId) {
-    client.getRichMenuIdOfUser(userId).then(function (richMenuId) {
-        return richMenuId;
+    return new Promise(function (resolve, reject) {
+        client.getRichMenuIdOfUser(userId).then(function (richMenuId) {
+            resolve(richMenuId);
+        });
     }).catch(function (e) {
-        console.log("getRichMenuIdOfUser " + userId + " error:" + e);
+        console.log('getRichMenuIdOfUser ' + userId + ' error:' + e);
+        reject('getRichMenuIdOfUser ' + userId + ' error:' + e);
     });
 }
 
 //取得群組中成員
 exports.getGroupMemberIds = function (groupId) {
-    client.getGroupMemberIds(groupId).then(function (ids) {
-        return ids;
+    return new Promise(function (resolve, reject) {
+        client.getGroupMemberIds(groupId).then(function (ids) {
+            resolve(ids);
+        });
     }).catch(function (e) {
-        console.log("getGroupMemberIds " + groupId + " error:" + e);
+        console.log('getGroupMemberIds ' + groupId + ' error:' + e);
+        reject('getGroupMemberIds ' + groupId + ' error:' + e);
     });
 }
 
 //離開群組
 exports.leaveGroup = function (groupId) {
-    client.leaveGroup(groupId).then(function () {
+    return new Promise(function (resolve, reject) {
+        client.leaveGroup(groupId).then(function () {
+            resolve(200);
+        });
     }).catch(function (e) {
-        console.log("leaveGroup " + groupId + " error:" + e);
+        console.log('leaveGroup ' + groupId + ' error:' + e);
+        reject('leaveGroup ' + groupId + ' error:' + e);
     });
 }
