f36879b020a6d94c7299d04ebc922467f716829b
s961744 <A0012272@cppcb.com.tw>
2018-07-19 13:40:37 +0800

    Add files via upload

--- js/request.js
+++ js/request.js
@@ -48,6 +48,48 @@ exports.requestHttpGet = function (url) {
 }
 
 /**
+* 處理http GET with req
+* @param {String} url
+*/
+exports.requestHttpGetWithReqData = function (url, reqData) {
+    return new Promise(function (resolve, reject) {
+        //解析 url 地址
+        var urlData = urltil.parse(url);
+        //設定 http.request options 傳入的參數
+        var options = {
+            //目標主機地址
+            hostname: urlData.hostname,
+            //目標地址
+            path: urlData.path,
+            //目標PORT
+            port: urlData.port,
+            //請求方法
+            method: 'GET'
+        };
+        var req = http.request(options, function (res) {
+            var buffer = [], result = '';
+            //監聽data事件 接收資料
+            res.on('data', function (data) {
+                buffer.push(data);
+            });
+            //監聽end事件 完成資料的接收
+            res.on('end', function () {
+                result = Buffer.concat(buffer).toString('utf-8');
+                resolve(result);
+            })
+        })
+            //監聽error事件
+            .on('error', function (err) {
+                console.log(err);
+                reject(err);
+            });
+        //傳入資料
+        req.write(reqData);
+        req.end();
+    });
+}
+
+/**
 * 處理http POST
 * @param {String} url
 */
@@ -78,11 +120,11 @@ exports.requestHttpPost = function (url, data) {
                 resolve(result);
             })
         })
-            //監聽error事件
-            .on('error', function (err) {
-                console.log(err);
-                reject(err);
-            });
+        //監聽error事件
+        .on('error', function (err) {
+            console.log(err);
+            reject(err);
+        });
         //傳入資料
         req.write(data);
         req.end();
