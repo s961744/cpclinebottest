30f4f496ba1aef09ace0ccb966c3c26b413d55ac
s961744 <A0012272@cppcb.com.tw>
2018-07-19 16:34:38 +0800

    Add files via upload

--- js/request.js
+++ js/request.js
@@ -52,7 +52,7 @@ exports.requestHttpGet = function (url) {
 * @param {String} url
 */
 exports.requestHttpGetWithReqData = function (url) {
-    console.log(url);
+    console.log('http get:' + url);
     return new Promise(function (resolve, reject) {
         http.get(url, function (res) {
             var body = '';
