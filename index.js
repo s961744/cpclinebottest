3560b909bd47d58b2adb3f9967575fea5d0ee61b
s961744 <A0012272@cppcb.com.tw>
2018-07-20 10:47:55 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -31,13 +31,13 @@ app.get('/json/url.json', function (req, res) {
     var url = 'http://116.50.39.201:7102/LineRESTful/resources/AttendanceAPI';
     var path = "/2018-07-19/1300";
     request.requestHttpGetJson(url + path).then(function (data) {
-        console.log('data=' + JSON.stringify(data));
-        console.log('attendance=' + JSON.stringify(data.attendance));
+        //console.log('data=' + JSON.stringify(data));
+        //console.log('attendance=' + JSON.stringify(data.attendance));
         //var obj = objsArray.filter(function (msg) {
         //    return msg.msgName == msgName;
         //});
         //resolve(obj[0].msg);
-        res.send(data.attendance);
+        res.send(data);
     });
 });
 
