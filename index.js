745bc91807e2975bd08a62c2e5c1d0764b499f68
s961744 <A0012272@cppcb.com.tw>
2018-08-09 16:19:01 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -68,8 +68,8 @@ var server = app.listen(process.env.PORT || 8080, function () {
     console.log('App now running on port', port);
 });
 
-// 排程 1次/15sec
-var job = schedule.scheduleJob('5,15,15,35,45,55 * * * * *', function () {
+// 排程 1次/10sec
+var job = schedule.scheduleJob('5,15,25,35,45,55 * * * * *', function () {
     // 取得line_message_send中的待發訊息並發送
     request.getUrlFromJsonFile('lineRESTful').then(function (url) {
         request.requestHttpGetJson(url).then(function (data) {
