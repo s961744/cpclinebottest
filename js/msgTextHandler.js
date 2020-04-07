813a61337de2947fcca83ab646963198d643d23e
s961744 <A0012272@cppcb.com.tw>
2020-04-07 16:49:42 +0800

    Add files via upload

--- js/msgTextHandler.js
+++ js/msgTextHandler.js
@@ -7,6 +7,7 @@ exports.msgTextHandle = function (event) {
     //個人ID
     if (event.message.text.toUpperCase() === 'ID' && event.source.type === 'user') {
         msg.getMsgFromJsonFile('msg', 'emptyMsg').then(function (msgData) {
+            console.log('msgData=' + JSON.stringify(msgData));
             msgData.text = event.source.userId;
             lineBotSdk.replyMessage(event.replyToken, msgData);
         });
@@ -14,6 +15,7 @@ exports.msgTextHandle = function (event) {
     //群組ID
     else if (event.message.text.toUpperCase() === 'ID' && event.source.type === 'group') {
         msg.getMsgFromJsonFile('msg', 'emptyMsg').then(function (msgData) {
+            console.log('msgData=' + JSON.stringify(msgData));
             msgData.text = event.source.groupId;
             lineBotSdk.replyMessage(event.replyToken, msgData);
         });
