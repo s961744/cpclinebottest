49ab2f389d2c1d30760f9b77840ad42a18d9eac7
s961744 <A0012272@cppcb.com.tw>
2020-04-07 10:21:41 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ const
 //}, 1500000); // every 25 minutes (1500000)
 
 const app = express();
-
+app.configure(function(){
+    app.use(express.bodyParser());
+    app.use(app.router);
+  });
+  
 // 因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
